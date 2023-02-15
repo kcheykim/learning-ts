dd1b1741db17c6dde9fd5c4ccf140591ebece1df
kcheykim <kosalcheykim@gmail.com>
2023-02-14 22:42:11 -0800

    adding styles and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,60 @@
 import React from 'react';
 import './App.css';
 
+let name: string;
+let age: number | string;
+let isStudent: boolean;
+let hobbies: string[];
+let role: [number,string];
 
-function App() {
+//let x: unknown;
 
+let printName: (name: string)  => never; // => void
+
+//printName('Yolo');
+
+// type Person = {
+//   name: string;
+//   age?: number;
+// };
+
+// let person: Person = {
+//   name: 'Yolo',
+// };
+
+// let lotsOfPeople: Person[];
+
+interface Person {
+  name: string;
+  age?: number;
+}
+
+// interface Guy extends Person {
+//   profession: string;
+// }
+
+type X = Person & {
+  a: string;
+  b: number;
+};
+
+// type Y = X & {
+//   c: string;
+//   d: number;
+// };
+
+// let y: Y = {
+//   c: 'efda',
+//   d: 42,
+// };
+
+const App: React.FC = () => {
   return (
     <div className="App">
-      Hello World!
+      <span className='heading'>Taskify</span>
+      <InputField />
     </div>
-  );
-  
-}
+  ); 
+};
 
 export default App;
